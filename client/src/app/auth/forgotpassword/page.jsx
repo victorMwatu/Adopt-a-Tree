5a74abd6d91eda288ede7539966a2c93d5d1cf62
victorMwatu <victormwatu@gmail.com>
2025-10-29 13:38:41 +0300

    Update API endpoint for forgot password feature

--- client/src/app/auth/forgotpassword/page.jsx
+++ client/src/app/auth/forgotpassword/page.jsx
@@ -14,7 +14,7 @@ export default function ForgotPasswordPage() {
     setMessage("");
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/api/auth/forgotpassword", {
+      const res = await fetch("https://adopt-a-tree.onrender.com/api/auth/forgotpassword", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
