47f3b9880d34fac66adb26814c6301a14997b8f4
victorMwatu <victormwatu@gmail.com>
2025-10-29 12:07:21 +0300

    Deploy: Update environment variable names in config

--- server/config.py
+++ server/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
+    SECRET_KEY = os.getenv('fletchermichaelvictor', 'dev-secret-key')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://adoptatree_db_user:JyGm81b3rQFpkS3VGfb9gbsZemmBdIgj@dpg-d40t8pn5r7bs7387jde0-a/adoptatree_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt-secret-key')
+    JWT_SECRET_KEY = os.getenv('fletchermichaelvictor', 'jwt-secret-key')
